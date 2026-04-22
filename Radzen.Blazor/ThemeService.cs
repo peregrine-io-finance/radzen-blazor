327c235b01b0066dadf51830aeb62b251129a3dd
Garrett Womack <garrett.womack@peregrine.io>
2026-04-22 11:53:51 -0700

    Scaffold Peregrine theme from Material theme.  The peregrine theme will be customized to the Peregrine.io branding template

--- Radzen.Blazor/ThemeService.cs
+++ Radzen.Blazor/ThemeService.cs
@@ -525,6 +525,8 @@ namespace Radzen
 
         internal bool Embedded => Theme switch
         {
+            "peregrine" => true,
+            "peregrine-dark" => true,
             "material" => true,
             "material-dark" => true,
             "standard" => true,
